87987f347ac07804e99b39c14f482c2f2c2d782c
Yash Tyagi <yashtyagi8006@gmail.com>
2025-09-21 12:22:26 +0530

    splash Screen code

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SizedBox(
+          height: 300,
+          width: 300,
+          child: Image.asset("assets/images/todologo.png", fit: BoxFit.fill),
+        ),
+      ),
+    );
+  }
+}
