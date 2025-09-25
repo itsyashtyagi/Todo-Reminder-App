161dd583088ec86a94dcb473ad5068bb6d02719a
Yash Tyagi <yashtyagi8006@gmail.com>
2025-09-25 08:26:37 +0530

    .env file code added

---
+++ lib/constant/api_constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiConstant {
+  static String baseUrl = dotenv.env['API_BASE_URL'] ?? "";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/app.dart';
 import 'package:todoapp/provider/login_provider.dart';
 import 'package:todoapp/provider/splash_provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   runApp(
     MultiProvider(
       providers: [
